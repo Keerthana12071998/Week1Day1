fbfba481d074356ac7443d8e7c84b41b2ee39775
Keerthana12071998 <nitaigauralove98@gmail.com>
2026-05-25 15:44:15 +0530

    Add files via upload

---
+++ 01-Odd-Even.js
@@ -0,0 +1,10 @@
+console.log("Odd/Even")
+let isOddOrEven = 25
+if(isOddOrEven%2 === 0)
+{
+console.log(`${isOddOrEven} is an Even number`)
+}
+else
+{
+    console.log(`${isOddOrEven} is a Odd number`)
+}
