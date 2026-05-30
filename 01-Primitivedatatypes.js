866c062b998b81f17c845011a8cf30487c744b80
Keerthana12071998 <nitaigauralove98@gmail.com>
2026-05-30 08:17:13 +0530

    Add files via upload

---
+++ 01-Primitivedatatypes.js
@@ -0,0 +1,11 @@
+console.log("PrimitiveDatatypes")
+let firstName = "Keerthana"
+let companyName = "IBM"
+let mobileNumber = 9876543210
+let isAutomation = true
+let hasPlaywright
+console.log(typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
