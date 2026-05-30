367af4b0a3d017b5db740b9ab2d1da749964384e
Keerthana12071998 <nitaigauralove98@gmail.com>
2026-05-30 08:17:35 +0530

    Add files via upload

---
+++ 01-var-let-conts_doubt.js
@@ -0,0 +1,11 @@
+const browserVersion = "Chrome"
+function getBrowserVersion(browserVersion)
+{
+    if(browserVersion === "Chrome")
+    {
+        //var browserVersion = "new"
+        let browserVersion = "new"
+    }
+    console.log(browserVersion)
+}
+getBrowserVersion(browserVersion)
